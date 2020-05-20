0686714137ea2b0efa1daf69d5dd4b4a72057914
Isaac Silva <isaac.silva@usach.cl>
2020-05-20 06:33:00 -0400

    Isaac: Ignoring IDEA conf files. Also, adding Version.java to source path.

--- modules/core/src/main/java/org/terrier/Version.java
+++ modules/core/src/main/java/org/terrier/Version.java
@@ -7,7 +7,7 @@ package org.terrier;
  */
 public class Version {
 
-	public static final String VERSION = "${project.version}";
+	public static final String VERSION = "5.2";
 
 	public static int getMajorVersion()
 	{
